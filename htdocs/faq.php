718716a6fe65d92a4d64b4e662067e463f82fa72
TadTakin <takintadayoni@yahoo.com>
2019-02-28 00:27:55 -0500

    FAQ Page Added
    CSS rustiness made this take way too long, but it's a half decent start with dummy values for the FAQs. [Do action] will be filled in once I figure out how Grid will be interacting with links on the page.

---
+++ htdocs/faq.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="faqstyle.css"
+  //Use a single forward slash if style sheet is in folder above
+  type="text/css" />
+  <title>LC Technologies FAQ</title>
+  <div id="FAQIntro" class="faqlist">
+    <div id="FAQIntroBGImage"></div>
+    <h2> If you see your issue listed below, please [do action] </h2>
+    <h2> Can't find your problem on this page? Try going back and using the Interactive Help Tool </h2> <br>
+</div>
+</head>
+<body>
+
+<div id="LeftCol" class="faqlist">
+<ul>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+</ul>
+</div>
+
+<div id="RightCol" class="faqlist">
+<ul>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+  <li><a href="https://localhost">What should I do when my wireless keyboard isn't working?</a></li>
+</ul>
+</div>
+
+</body>
+</html>
