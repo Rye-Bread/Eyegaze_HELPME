2ffa6142daec5a8543bca724f617cfcaf13be82e
TadTakin <takintadayoni@yahoo.com>
2019-03-13 20:59:15 -0400

    Restoring FIles

---
+++ htdocs/SusanFAQ.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="susanfaqstyle.css"
+  //Use a single forward slash if style sheet is in folder above
+  type="text/css" />
+  <title>LC Technologies FAQ</title>
+  <div id="FAQIntro" class="faqlist">
+    <div id="FAQIntroBGImage"></div>
+    <h2> If you see your issue listed below, please [do action] </h2>
+    <h2> Can't find your problem on this page? Try going back and using the Interactive Help Tool </h2> <br>
+</div>
+</head>
+<body>
+
+<div id="LeftCol" class="faqlist">
+	<header><strong>HARDWARE</strong></header>
+	<ol type="1">		
+	  <li><a href="http://localhost/calibration.php">Issues with calibration</a></li>
+	  <li><a href="https://localhost/keyboard_connectivity.php">Keyboard Connectivity</a></li>
+	  <li><a href="https://localhost/camera_connectivity.php">Camera Connectivity</a></li>
+	  <li><a href="https://localhost/error_message.php">Error Messages</a></li>
+	  <li><a href="https://localhost/battery_issues.php">Issues with the Battery</a></li>
+	  <li><a href="https://localhost/edge_links.php">How to set up Edge Links</a></li>
+	</ol>
+</div>
+
+<div id="RightCol" class="faqlist">
+	<header><strong>SOFTWARE</strong></header>
+	<ol type="1">	
+	  <li><a href="https://localhost/grid_issue.php">Issues with Grid</a></li>
+	  <li><a href="https://localhost/network_issue.php">Network Issues</a></li>
+	</ol>	
+</div>
+
+</body>
+</html>
