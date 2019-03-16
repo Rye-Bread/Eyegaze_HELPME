9bf77b013cc70de17e52a1d6d9333486c3b4a04f
michael <chheam24@gmail.com>
2019-03-16 13:43:55 -0400

    update FAQ edge link

--- htdocs/edge_links.php
+++ htdocs/edge_links.php
@@ -6,14 +6,40 @@
   type="text/css" />
   <title>LC Technologies</title>
   <div id="faq-title" class="faq-title-area">
-    <h2> How to set up Edge Links </h2>
+    <h2> How to set Up Edge Links </h2>
  </h2> <br>
 </div>
 </head>
-<body>
+<body> 
+	
+
+<li>[INSERT Eyegaze Edge Link.mp4 HERE]</li>
+<li>Eyegaze Edge® Link Set-Up Instructions: </li>
+<ol type="1">
+<li>If this is the first time setting up the Edge Link on your system you need to:</li>
+<li> Make sure the Eyegaze System is running the latest version of software (3-12-13 or later).</li>
+<li>Before inserting the link into the Eyegaze system change your PC Access method in the</li>
+<li>Eyegaze Settings</li>
+<li>Open the “Eyegaze Edge Settings” icon on your desktop</li>
+<br>
+			
+			<div id="Communication-system">
+				<img src="img/communication_system.png" alt="communication">	
+					<br>
+
+<li>Select the ‘PC Access” tab</li>
+<li>Pull down the “Comm Link” menu and select “Eyegaze Edge</li>
+<li>Link.” Apply change and close the settings window.</li>
+<li>Plug one Link into your Eyegaze System.</li>
+<li>Plug the 2nd Link into your Personal Computer (PC).</li>
+<li>Wait for your PC and/or Eyegaze System to load the drivers automatically.</li>
+<li>You will hear a ding sound telling you the PC and/or Eyegaze System has installed the drivers for the Eyegaze Edge Link.</li>
+
+
+
 
 <div id="faq-body" class="faq-body-area">
-[BODY]
+
 </div>
 
 
