b77c0a2665a6c1ff7adc4dec76f14d66aae76966
TadTakin <takintadayoni@yahoo.com>
2019-04-19 23:45:30 -0400

    Fixes and adds
    Added topics to software side, fixed typo in footer, fixed spacing on index page

--- htdocs/faqsmainpage.php
+++ htdocs/faqsmainpage.php
@@ -6,7 +6,6 @@
   <link rel="stylesheet" href="knowledgebase.css" type="text/css" />
   <link rel="stylesheet" type="text/css" href="footer.css" />
   <link rel="icon" href="img/eyeicon.png" type="image/gif" sizes="20x16">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
 
   <title>FAQs</title>
 
@@ -45,19 +44,19 @@
 			Access keys can only be one character and cannot be repeated on the same page. Each link needs a unique character -->
 					  <li><a href="calibration.php" accesskey="1">
 						  Having issues with calibration?</a></li>
-						  
+
 					  <li><a href="keyboard_connectivity.php" accesskey="2">
 						  Problems with keyboard connectivity?</a></li>
-						  
+
 					  <li><a href="topics.php?Title=Camera%20Connectivity" accesskey="3">
 						  Problems with camera connectivity?</a></li>
-						  
+
 					  <li><a href="error_messages.php" accesskey="4">
 						  Receiving error messages?</a></li>
-						  
+
 					  <li><a href="topics.php?Title=Battery%20Life" accesskey="5">
 						  Having issues with the battery?</a></li>
-						  
+
 					  <li><a href="edge_links.php" accesskey="6">
 						  Need assistance setting up Edge Links?</a></li>
 					</ol>
@@ -68,10 +67,17 @@
 		<header id="FAQsubtitle">SOFTWARE</header>
 		<ol type="a">
 		  <li><a href="grid_issues.php" accesskey="a">
-			  Having issues with Grid?</a></li>
-			  
+			  Having issues with accessing email through your browser?</a></li>
 		  <li><a href="networking_issues.php" accesskey="b">
-			  Having network issues?</a></li>
+        Having issues with accessing email through Grid?</a></li>
+      <li><a href="networking_issues.php" accesskey="c">
+  			Trouble with texting or phone calls?</a></li>
+      <li><a href="networking_issues.php" accesskey="d">
+        Problems with setting up the WhatsApp desktop client?</a></li>
+      <li><a href="networking_issues.php" accesskey="e">
+        Can't connect to the internet?</a></li>
+      <li><a href="networking_issues.php" accesskey="f">
+      Encountering errors while entering custom phrases?</a></li>
 		</ol>
 	</div>
 
--- htdocs/index.php
+++ htdocs/index.php
@@ -6,7 +6,6 @@
   <link rel="stylesheet" href="knowledgebase.css" type="text/css" />
   <link rel="stylesheet" type="text/css" href="footer.css" />
   <link rel="icon" href="img/eyeicon.png" type="image/gif" sizes="20x16">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
 
   <title>Eyegaze Edge Knowledge Base</title>
 
@@ -32,18 +31,21 @@
 	<div id="FAQtitle">
 		Hello and welcome to the Eyegaze Edge Knowledge Base!<br>
 	</div>
+  <br>
 
 	<div id="IndexBody" class="IndexPage">
 		<h2> You can use the Grid buttons above to navigate through the Knowledge Base</h2>
-		<h2> Please select option A for the FAQ or option B for the Interactive Help Tool </h2>
+		<h2> Please select either the FAQ or the Interactive Help Tool using your Grid bar </h2>
     <div id = "IndexOptions" class="IndexPage">
-      <a href="faqsmainpage.php" accesskey="a">A - FAQ</a>
+      <a href="faqsmainpage.php" accesskey="a">FAQ</a>
+      <br><br>
       <p> The Frequently Asked Questions (FAQ) page provides a list of topics for you to choose from.
         If you already know the specifics or root cause of the issue with your Eyegaze Edge system,
         use the FAQ to go straight to the help topic.</p>
     </div>
     <div id = "IndexOptions" class="IndexPage">
-      <a href="selfhelpresource.php?Title=Hardware%20Issue" accesskey="b">B - Interactive Help Tool</a>
+      <a href="selfhelpresource.php?Title=Interactive%20Help" accesskey="b">Interactive Help Tool</a>
+      <br><br>
       <p> The Interactive Help Tool can help you narrow down the source of your issues. It can provide helpful suggestions,
         troubleshooting steps, and useful products for you to get the most out of your Eyegaze Edge experience. If you're having an
         issue and you're not sure why or how it is happening, use this tool and answer a series of questions to find your solution.</p>
